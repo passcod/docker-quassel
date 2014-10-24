adf8088d5421c65a5b191ef192ed4cccd49fdafa
Félix Saparelli <felix@passcod.name>
2014-10-24 14:25:42 +1300

    Fix script

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-chown -R quasselcore:users /var/lib/quassel
-chmod -R g+rwX /var/lib/quassel
-su -c '/usr/bin/quassel --configdir=/var/lib/quassel $@' quasselcore
+if [ "$(id -u)" != "0" ]; then
+  /usr/bin/quasselcore --configdir=/var/lib/quassel/ $@
+else
+  chown -R quasselcore:users /var/lib/quassel
+  chmod -R g+rwX /var/lib/quassel
+  sudo -u quasselcore /start $@
+fi
